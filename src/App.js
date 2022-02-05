33215279723d456cac37cd397a2f11868e610442
LukasAxelborn <lukasaxelborn@live.se>
2022-02-05 12:22:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> kul att det funakar! hej på difS.
         </p>
         <a
           className="App-link"
